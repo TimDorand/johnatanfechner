86f263a3f76f4c0dac8a2f508cbe199ffd99ac7c
timothee <tim.dorand@gmail.com>
2015-11-16 18:46:22 +0100

    Début de la Landing Page

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tim
+* Date: 16/11/2015
+* Time: 16:52
+*/
+
+require('headerhome.php');
+
+?>
+
+    <div class="col-md-12 head">
+
+    <h1><strong>Jonathan Fechner</strong><span class="soustitre">Production & Developpement multimédia</span></h1>
+        <div class="col-md-12 masteries">
+    <div class="master"><span class="glyphicon glyphicon-headphones"></span></div>
+    <div class="master"><span class="glyphicon glyphicon-user"></span></div>
+    <div class="master"><span class="glyphicon glyphicon-phone"></span></div>
+    <div class="master"><span class="glyphicon glyphicon-bullhorn"></span></div>
+    <div class="master"><span class="glyphicon glyphicon-hand-right"></span></div>
+
+    </div>
+    </div>
+</div>
+
+    <div class="col-md-12">
+        <h2>A propos</h2>
+        <p>Jonathan Fechner est Consultant Indépendant Digital spécialiste des techniques de l'information et de la communication.
+            Il trouve des solutions multimédia pour les entreprises.
+            <a href="apropos.php">En savoir plus...</a>
+        </p>
+    </div>
+
+    <div class=""></div>
+
+
+
+<!--<footer>
+
+</footer>-->
+
+
+</body>
+</html>
