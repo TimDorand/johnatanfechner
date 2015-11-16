cda48bb6c148009fd3679592358d7ae1ec47a58d
timothee <tim.dorand@gmail.com>
2015-11-16 19:58:20 +0100

    20h landing page

--- index.php
+++ index.php
@@ -14,17 +14,18 @@ require('headerhome.php');
 
     <h1><strong>Jonathan Fechner</strong><span class="soustitre">Production & Developpement multimédia</span></h1>
         <div class="col-md-12 masteries">
-    <div class="master"><span class="glyphicon glyphicon-headphones"></span></div>
-    <div class="master"><span class="glyphicon glyphicon-user"></span></div>
-    <div class="master"><span class="glyphicon glyphicon-phone"></span></div>
-    <div class="master"><span class="glyphicon glyphicon-bullhorn"></span></div>
-    <div class="master"><span class="glyphicon glyphicon-hand-right"></span></div>
+    <div class="mas"><div class="col-md-12 master"><span class="glyphicon glyphicon-headphones"></span></div><p>Ecoute</p></div>
+    <div class="mas"><div class="col-md-12 master"><span class="glyphicon glyphicon-user"></span></div><p>Conseil</p></div>
+    <div class="mas"><div class="col-md-12 master"><span class="glyphicon glyphicon-phone"></span></div><p>Innovation</p></div>
+    <div class="mas"><div class="col-md-12 master"><span class="glyphicon glyphicon-bullhorn"></span></div><p>Gestion de Projets</p></div>
+    <div class="mas"><div class="col-md-12 master"><span class="glyphicon glyphicon-hand-right"></span></div><p>Flatteries</p></div>
 
     </div>
     </div>
 </div>
-
-    <div class="col-md-12">
+<div class="container-fluid sommaire">
+<div class="row">
+    <div class="col-md-6 somm">
         <h2>A propos</h2>
         <p>Jonathan Fechner est Consultant Indépendant Digital spécialiste des techniques de l'information et de la communication.
             Il trouve des solutions multimédia pour les entreprises.
@@ -32,10 +33,33 @@ require('headerhome.php');
         </p>
     </div>
 
-    <div class=""></div>
+    <div class="col-md-6 ">
+        <h2>Cours, Avant-Projets </h2>
+        <p>Jonathan Fechner est Consultant Indépendant Digital spécialiste des techniques de l'information et de la communication.
+            Il trouve des solutions multimédia pour les entreprises.
+            <a href="apropos.php">En savoir plus...</a>
+        </p>
+    </div>
+</div>
+    <div class="row">
+    <div class="col-md-6 ">
+        <h2>Cours, Gestion de projet </h2>
+        <p>Jonathan Fechner est Consultant Indépendant Digital spécialiste des techniques de l'information et de la communication.
+            Il trouve des solutions multimédia pour les entreprises.
+            <a href="apropos.php">En savoir plus...</a>
+        </p>
+    </div>
 
+    <div class="col-md-6 ">
 
+    </div>
+    </div>
 
+    <div class="col-md-6 ">
+
+    </div>
+
+</div>
 <!--<footer>
 
 </footer>-->
