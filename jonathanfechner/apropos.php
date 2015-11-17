6e0a39301e004bded73087eb6dd04ddece6f3ca1
Jérémie Dahan <jeremied75@gmail.com>
2015-11-17 11:20:36 +0100

    apropos

---
+++ jonathanfechner/apropos.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Jonathan Fechner</title>
+    <link rel="icon" type="image/png" href="" />
+
+<!-- Dublin Core -->
+    <meta name="DC.title" content="Site Tag Heuer,montres de luxe pour homme et femme">
+    <meta name="DC.creator" content="Jérémie Dahan">
+    <meta name="DC.subject" content="Montres, Luxe, bijoux, tag heuer, tagheuer" />
+    <meta name="DC.description" content="Tag Heuer, Créateur de montre suisse de luxe depuis 1860. Modeles pour hommes et femmes, achat en boutique, vitrine en ligne, luxe, horlogerie" />
+    <meta name="DC.publisher" content="Jérémie Dahan">
+    <meta name="DC.format" content="website">
+    <meta name="DC.identifier" content="www.gorillazhd.com">
+    <meta name="DC.language" content="fr-FR">
+    <meta name="DC.coverage" content="World">
+    <meta name="DC.rights" content="&copy; Tag Heuer">
+    <!-- END Dublin Core -->
+
+
+    <!-- Referencement -->
+    <meta name="description" content="Tag Heuer, Créateur de montre suisse de luxe depuis 1860. Modeles pour hommes et femmes, achat en boutique, vitrine en ligne, luxe, horlogerie">
+    <meta name="keywords" content="Montres, Luxe, bijoux, tag heuer, tagheuer">
+    <meta name="author" content="Jérémie Dahan">
+    <meta name="robots" content="index">
+    <meta name="Indentifier-URL" content="www.gorillazhd.com">
+    <!-- END Référencement -->
+
+
+    <!-- Open Graph-->
+    <meta property="og:title" content="Tag Heuer : Montres de luxe pour Hommes et Femmes">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="www.gorillazhd.com">
+    <meta property="og:image" content="www.gorillazhd.com">
+    <meta property="og:site_name" content="Tag Heuer">
+    <meta property="og:description" content="Tag Heuer, Créateur de montre suisse de luxe depuis 1860. Modeles pour hommes et femmes, achat en boutique, vitrine en ligne, luxe, horlogerie">
+    <!-- END Open Graph-->
+
+
+
+
+
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css"  href="css/bootstrap.css">
+
+
+
+    <!-- Stylesheet
+    ================================================== -->
+    <link rel="stylesheet" type="text/css"  href="css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+
+
+
+</head>
+
+
+
+<body>
+
+    <p class=" text-center"> <h1>A PROPOS</h1></p>
+<div class= "container-fluid  profil">
+<div class="col-xs-4 col-md-offset-1 photo"> <img src="img/profil.jpg" class="img-responsive profil" alt="Responsive image"> </div>
+<div class="col-xs-5">  <h4>Jonathan Fechner est Consultant Indépendant Digital spécialiste des techniques de l'information et de la communication. Il trouve des solutions multimédia pour les entreprises. Son intervention sur vos projets et avant-projets permet de vous apporter des réponses claires, du cahier des charges fonctionnel au succès de l'opération.
+
+Vous êtes une entreprise ou un porteur de projet, grâce à son réseau de plus de 200 professionnels reconnus, Jonathan Fechner peut constituer une équipe dédiée pour garantir le succès de votre projet. Opération de communication, création de site web, 3D, print, ou vidéo, faire appel à Jonathan Fechner c’est gagner un temps précieux et la certitude d’atteindre vos objectifs.</h4> </div>
+
+</div>
+<div class="container-fluid col-xs-12 ">
+  <div class="container-fluid row2">
+    <div class="col-xs-3 col-md-offset-1 text-center">
+<h3>Gestion de Projet gagnez du temps et de l’argent</h3>
+
+<p class="text-justify"> Expert en Gestion de projet. Diagramme de Gantt, Scrum Master, méthode Agile recrutement de spécialistes, gestion des ressources. Profitez d’un réseau de plus de 200 professionnels reconnu. Vous avez un projet, il trouve les solutions.</p>
+</div>
+    <div class="col-xs-3 text-center ">
+<h3>Design du papier à l'expérience utilisateur</h3>
+
+<p class="text-justify">Expert en comportements humains. Ergonomie, PNL ou communication classique, il faut se mettre à la place de l’utilisateur pour lui offrir ce qu’il attend. Vous voulez comprendre comment augmenter les ventes de votre site de e-commerce ? Il vous permet d’atteindre vos objectifs.</p>
+</div>
+
+    <div class="col-xs-3 text-center">
+<h3>Ingénierie de la technique au multimédia</h3>
+
+<p class="text-justify">Expert en Design. Vous avez besoin d’une maquette ou d’un modèle de pré série ? Prototypage rapide, impression 3D. Vous voulez un site internet ? Quelle technologie choisir pour votre projet ? Il vous apporte la solution.</p>
+</div>
+  </div>
+
+
+  <div class="row3">
+    <div class="col-md-4 col-md-offset-1 text-justify"><h3>De la gestion de projet au succès de l’opération</h3> Après 15 ans d'expérience dans la gestion de projets, et 12 années à la tête d'entreprises, Jonathan Fechner ne cesse de chercher à améliorer les processus, de l'idée au projet abouti. Fort de ces expériences, il conseil aujourd’hui des entreprises de renommée international mais accompagne aussi de jeunes créateurs d’entreprises dans leurs projets.</p></p></div>
+    <div class="col-md-6 col-md-offset-1"> <h3>Ses compétences<h3> <div class="progress">
+  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+    <span class="sr-only">40% Complete (success)</span>
+  </div>
+</div>
+<div class="progress">
+  <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+    <span class="sr-only">20% Complete</span>
+  </div>
+</div>
+<div class="progress">
+  <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+    <span class="sr-only">60% Complete (warning)</span>
+  </div>
+</div>
+<div class="progress">
+  <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+    <span class="sr-only">80% Complete (danger)</span>
+  </div>
+ </div>
+  </div>
+
+
+
+</body>
+</html>
---
+++ jonathanfechner/johnatanfechner/index.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tim
+* Date: 16/11/2015
+* Time: 16:52
+*/
+
+require('headerhome.php');
+
+?>
+
+    <div class="col-md-12 head">
+
+    <h1><strong>Jonathan Fechner</strong><span class="soustitre">Production & Developpement multimédia</span></h1>
+        <div class="col-md-12 masteries">
+    <div class="mas"><div class="col-md-12 master"><span class="glyphicon glyphicon-headphones"></span></div><p>Ecoute</p></div>
+    <div class="mas"><div class="col-md-12 master"><span class="glyphicon glyphicon-user"></span></div><p>Conseil</p></div>
+    <div class="mas"><div class="col-md-12 master"><span class="glyphicon glyphicon-phone"></span></div><p>Innovation</p></div>
+    <div class="mas"><div class="col-md-12 master"><span class="glyphicon glyphicon-bullhorn"></span></div><p>Gestion de Projets</p></div>
+    <div class="mas"><div class="col-md-12 master"><span class="glyphicon glyphicon-hand-right"></span></div><p>Flatteries</p></div>
+
+    </div>
+    </div>
+</div>
+<div class="container-fluid sommaire">
+<div class="row">
+    <div class="col-md-6 somm">
+        <h2>A propos</h2>
+        <p>Jonathan Fechner est Consultant Indépendant Digital spécialiste des techniques de l'information et de la communication.
+            Il trouve des solutions multimédia pour les entreprises.
+            <a href="apropos.php">En savoir plus...</a>
+        </p>
+    </div>
+
+    <div class="col-md-6 ">
+        <h2>Cours, Avant-Projets </h2>
+        <p>Jonathan Fechner est Consultant Indépendant Digital spécialiste des techniques de l'information et de la communication.
+            Il trouve des solutions multimédia pour les entreprises.
+            <a href="apropos.php">En savoir plus...</a>
+        </p>
+    </div>
+</div>
+    <div class="row">
+    <div class="col-md-6 ">
+        <h2>Cours, Gestion de projet </h2>
+        <p>Jonathan Fechner est Consultant Indépendant Digital spécialiste des techniques de l'information et de la communication.
+            Il trouve des solutions multimédia pour les entreprises.
+            <a href="apropos.php">En savoir plus...</a>
+        </p>
+    </div>
+
+    <div class="col-md-6 ">
+
+    </div>
+    </div>
+
+    <div class="col-md-6 ">
+
+    </div>
+
+</div>
+<!--<footer>
+
+</footer>-->
+
+
+</body>
+</html>
